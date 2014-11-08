5c723f892221e829abb7bfba98c46ebe984d1f96
Elekron <andreasbolzyk@andreass-mbp.lan>
2014-11-09 00:31:57 +0100

    d

--- js/getElement.js
+++ js/getElement.js
@@ -2,7 +2,7 @@ function getideFunction(title)
 {
 	xmlDoc=loadXMLDoc("databas.xml");
 
-	x=xmlDoc.getElementsByTagName("title")[0].childNodes[0];
+	x=xmlDoc.getElementsByTagName(title)[0].childNodes[0];
 	txt=x.nodeValue;
 	document.write(txt);
 }
