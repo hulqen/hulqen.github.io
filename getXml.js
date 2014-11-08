c6f9fdf8ce11678c59205fd78523c8787116ee12
Elekron <andreasbolzyk@andreass-mbp.lan>
2014-11-08 21:07:30 +0100

    dasfsaf

--- getXml.js
+++ getXml.js
@@ -1,22 +1,44 @@
-function allFunction() {
-  if (window.XMLHttpRequest)
-    {// code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp=new XMLHttpRequest();
-    }
-  else
-    {// code for IE6, IE5
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-  xmlhttp.open("GET","databas.xml",false);
-  xmlhttp.send();
-  xmlDoc=xmlhttp.responseXML; 
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.open("GET","databas.xml",false);
+xmlhttp.send();
+xmlDoc=xmlhttp.responseXML; 
 
 
-  var x=xmlDoc.getElementsByTagName("CD");
+var x=xmlDoc.getElementsByTagName("CD");
 
 
+
+function allFunction() 
+{
   for (i=0;i<x.length;i++)
-    { 
+  { 
+    document.write('<div id="productBox">');
+      document.write('<div id="productPicture">');
+        document.write('<img src="'+x[i].getElementsByTagName("IMG")[0].childNodes[0].nodeValue+'">');
+      document.write("</div>");
+
+      document.write('<div id="productDescription">');
+        document.write(x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue+"<br>");
+        document.write(x[i].getElementsByTagName("CREATOR")[0].childNodes[0].nodeValue+"<br>");
+        document.write(x[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue);
+      document.write("</div>");
+    document.write("</div>");
+  }
+}
+
+
+
+
+
+function oneElemetFunction(i) 
+{
   document.write('<div id="productBox">');
     document.write('<div id="productPicture">');
       document.write('<img src="'+x[i].getElementsByTagName("IMG")[0].childNodes[0].nodeValue+'">');
@@ -28,38 +50,20 @@ function allFunction() {
       document.write(x[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue);
     document.write("</div>");
   document.write("</div>");
-  }
-
 }
 
-function oneElemetFunction(i) {
-  if (window.XMLHttpRequest)
-    {// code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp=new XMLHttpRequest();
-    }
-  else
-    {// code for IE6, IE5
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-  xmlhttp.open("GET","databas.xml",false);
-  xmlhttp.send();
-  xmlDoc=xmlhttp.responseXML; 
-
-
-  var x=xmlDoc.getElementsByTagName("CD");
 
 
 
-document.write('<div id="productBox">');
-  document.write('<div id="productPicture">');
-    document.write('<img src="'+x[i].getElementsByTagName("IMG")[0].childNodes[0].nodeValue+'">');
-  document.write("</div>");
 
-  document.write('<div id="productDescription">');
-    document.write(x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue+"<br>");
-    document.write(x[i].getElementsByTagName("CREATOR")[0].childNodes[0].nodeValue+"<br>");
-    document.write(x[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue);
-  document.write("</div>");
-document.write("</div>");
 
-}
+
+
+
+
+
+
+
+
+
+
