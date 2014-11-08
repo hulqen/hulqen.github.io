68eed37c0357538cbcf90259d3125f0a70b76d7e
Elekron <andreasbolzyk@andreass-mbp.lan>
2014-11-08 23:47:37 +0100

    😌

--- js/getXml.js
+++ js/getXml.js
@@ -70,8 +70,30 @@ function loopElemetinlistFunction()
       document.write("</div>");
     document.write("</div>");
     document.write("</li>");
+  }
 }
+
+function loopNumElemetinlistFunction(j) 
+{
+  for (i=0;i<j;i++)
+    { 
+    document.write("<li>");
+    document.write('<div id="productBox">');
+      document.write('<div id="productPicture">');
+        document.write('<img src="'+x[i].getElementsByTagName("IMG")[0].childNodes[0].nodeValue+'">');
+      document.write("</div>");
+
+      document.write('<div id="productDescription">');
+        document.write(x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue+"<br>");
+        document.write(x[i].getElementsByTagName("CREATOR")[0].childNodes[0].nodeValue+"<br>");
+        document.write(x[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue);
+      document.write("</div>");
+    document.write("</div>");
+    document.write("</li>");
+  }
 }
+
+
 
 function imgFunction(i) 
 {
