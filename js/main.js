592affeaed7d6e64be72926781d5bf8527418117
Elekron <andreasbolzyk@andreass-mbp.lan>
2014-11-09 05:39:03 +0100

    dsfa

--- js/main.js
+++ js/main.js
@@ -43,4 +43,57 @@ function setGrid()
 }
 
 
+function getTag() 
+{
+  if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+  else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.open("GET","databas.xml",false);
+  xmlhttp.send();
+  xmlDoc=xmlhttp.responseXML; 
+
+  // xmlDoc=loadXMLDoc("books.xml");
+  for (i=0;i<3; i++)
+  {
+
+  x=xmlDoc.getElementsByTagName("TAG")[i]
+  y=x.childNodes[0];
+  document.write(y.nodeValue+"<br>");
+  }
+}
+
+
+function getImg(i) 
+{
+  document.write('<img src="'+x[i].getElementsByTagName("IMG")[0].childNodes[0].nodeValue+'">');
+}
+
+
+function getTitel(i) 
+{
+  document.write(x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue);
+}
+
+
+function getCreator(i) 
+{
+  document.write(x[i].getElementsByTagName("CREATOR")[0].childNodes[0].nodeValue);
+}
+
+
+function getPrice(i) 
+{
+  document.write(x[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue);
+}
+
+
+function getDescription(i)
+{
+  document.write(x[i].getElementsByTagName("DESCRIPTION")[0].childNodes[0].nodeValue)
+}
 
