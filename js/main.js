8271849fe9581f78faeb3ece6309b0a224878ed4
Satch3L <satch3l@gmail.com>
2014-11-09 06:38:10 +0100

    hh

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function loopElementInlist()
     { 
     document.write("<li>");
     document.write('<div id="productBox">');
-      document.write('<div id="productPicture">');
+      document.write('<div id="thumbnailPicture">');
         document.write('<img src="'+x[i].getElementsByTagName("IMG")[0].childNodes[0].nodeValue+'" alt="'+i+'">');
       document.write("</div>");
 
